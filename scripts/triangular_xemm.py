bd3e2ed3c38ac17fe1bb3c099268ca551c410c1a
supervik <vktsbk@gmail.com>
2022-12-23 00:25:17 +0300

    (feat) Triangular XEMM script - MVP - maker orders

--- scripts/triangular_xemm.py
+++ scripts/triangular_xemm.py
@@ -16,15 +16,15 @@ from hummingbot.strategy.script_strategy_base import Decimal, OrderType, ScriptS
 
 class TriangularXEMM(ScriptStrategyBase):
     # Config params
-    connector_name: str = "kucoin_paper_trade"
-    maker_pair: str = "ADA-BTC"
-    taker_pair_1: str = "BTC-USDT"
-    taker_pair_2: str = "ADA-USDT"
+    connector_name: str = "kucoin"
+    maker_pair: str = "XRP-USDT"
+    taker_pair_1: str = "XRP-BTC"
+    taker_pair_2: str = "BTC-USDT"
 
     min_spread: Decimal = Decimal("1")
     max_spread: Decimal = Decimal("2")
-    order_amount: Decimal = Decimal("10")
-    target_amount = Decimal("100")
+    order_amount: Decimal = Decimal("1")
+    target_amount = Decimal("0")
     # min_order_amount = Decimal("1")
     # slippage_buffer = Decimal("1")
 
@@ -35,6 +35,10 @@ class TriangularXEMM(ScriptStrategyBase):
     status: str = "NOT_INIT"
     taker_pairs: tuple = ()
     taker_order_sides: dict = {}
+    taker_price_calculation_method: int = 0
+    taker_sell_price = 0
+    taker_buy_price = 0
+    spread = 0
 
     has_open_bid = False
     has_open_ask = False
@@ -73,6 +77,9 @@ class TriangularXEMM(ScriptStrategyBase):
         self.check_target_balance()
 
         # open maker orders
+        self.taker_sell_price = self.calculate_taker_price(self.taker_order_sides["maker_bid_filled"])
+        self.taker_buy_price = self.calculate_taker_price(self.taker_order_sides["maker_ask_filled"])
+
         self.check_and_cancel_open_orders()
         self.place_maker_orders()
         
@@ -83,6 +90,8 @@ class TriangularXEMM(ScriptStrategyBase):
         self.status = "ACTIVE"
         self.set_trading_pair()
         self.set_order_side()
+        self.set_taker_price_calculation_method()
+        self.set_spread()
         
     # def check_trading_pair(self):
     #     """
@@ -127,141 +136,117 @@ class TriangularXEMM(ScriptStrategyBase):
         self.taker_order_sides["maker_bid_filled"] = (order_side_1, order_side_2)
         self.taker_order_sides["maker_ask_filled"] = (1 - order_side_1, 1 - order_side_2)
 
+    def set_taker_price_calculation_method(self):
+        taker_side_1, taker_side_2 = self.taker_order_sides["maker_bid_filled"][0], self.taker_order_sides["maker_bid_filled"][1]
+        if taker_side_1 == taker_side_2:
+            self.taker_price_calculation_method = 1
+        else:
+            self.taker_price_calculation_method = 2 if taker_side_2 else 3
+
+    def set_spread(self):
+        self.spread = (self.min_spread + self.max_spread) / 2
+
     def check_target_balance(self):
         pass
-    #     current_maker_base_balance = self.connector.get_available_balance(self.maker_base)
-    #     amount_diff = current_maker_base_balance - self.target_amount
-    #     amount_diff_quantize = self.connector.quantize_order_amount(self.maker_pair, Decimal(amount_diff))
-    #     if amount_diff_quantize == Decimal("0"):
-    #         return True
-    #     else:
-    #         # get min_order_size on taker market
-    #         min_order_size_taker_1 = self.connector()
-    #         min_order_size_taker_2 = self.connector()
-    #         if both > 0:
-    #             filled_side = "bid_filled" if amount_diff > 0 else "ask_filled"
-    #             self.log_with_clock(logging.INFO, f"Hedging mode started")
-    #             self.status = "HEDGE_MODE"
-    #             self.create_and_process_taker_order_candidates(filled_side, amount_diff_quantize)
-    #
+        # current_maker_base_balance = self.connector.get_available_balance(self.maker_base)
+        # amount_diff = current_maker_base_balance - self.target_amount
+        # amount_diff_quantize = self.connector.quantize_order_amount(self.maker_pair, Decimal(amount_diff))
+        # self.log_with_clock(logging.INFO, f"Current balance: {current_maker_base_balance}, "
+        #                                   f"Target balance: {self.target_amount}, "
+        #                                   f"amount_diff_quantize: {amount_diff_quantize}")
+        # if amount_diff_quantize == Decimal("0"):
+        #     return True
+        # else:
+        #     # get min_order_size on taker market
+        #     min_order_size_taker_1 = self.connector()
+        #     min_order_size_taker_2 = self.connector()
+        #     if both > 0:
+        #         filled_side = "bid_filled" if amount_diff > 0 else "ask_filled"
+        #         self.log_with_clock(logging.INFO, f"Hedging mode started")
+        #         self.status = "HEDGE_MODE"
+        #         self.create_and_process_taker_order_candidates(filled_side, amount_diff_quantize)
+
     def place_maker_orders(self):
-        pass
-        # if not self.has_open_bid:
-        #     # Calculate price
-        #     order_sides = self.taker_order_sides["maker_bid_filled"]
-        #     taker_1_price = self.connector.get_price(self.taker_pair_1, order_sides[0])
-        #     taker_2_price = self.connector.get_price(self.taker_pair_1, order_sides[1])
-        #     if order_sides[0] == order_sides[1]:
-        #         taker_price = taker_1_price * taker_2_price
-        #     else:
-        #         if order_sides[0]:
-        #             taker_price = taker_2_price / taker_1_price
-        #         else:
-        #             taker_price = taker_1_price / taker_2_price
-        #     result_price = taker_price -
-        #
-        # if not self.has_open_ask:
-        #     pass
+        if not self.has_open_bid:
+            order_price = self.taker_sell_price * Decimal(1 - self.spread / 100)
+            buy_candidate = OrderCandidate(
+                                trading_pair=self.maker_pair,
+                                is_maker=True,
+                                order_type=OrderType.LIMIT,
+                                order_side=TradeType.BUY,
+                                amount=self.order_amount,
+                                price=order_price)
+            buy_candidate_adjusted = self.connector.budget_checker.adjust_candidate(buy_candidate, all_or_none=False)
+            if buy_candidate_adjusted.amount > 0:
+                self.log_with_clock(logging.INFO, f"Buy {buy_candidate_adjusted.amount} {self.maker_pair} "
+                                                  f"with the price {buy_candidate_adjusted.price}")
+                self.buy(
+                    self.connector_name,
+                    self.maker_pair,
+                    buy_candidate_adjusted.amount,
+                    buy_candidate_adjusted.order_type,
+                    buy_candidate_adjusted.price)
+                self.has_open_bid = True
 
+        if not self.has_open_ask:
+            order_price = self.taker_buy_price * Decimal(1 + self.spread / 100)
+            sell_candidate = OrderCandidate(
+                trading_pair=self.maker_pair,
+                is_maker=True,
+                order_type=OrderType.LIMIT,
+                order_side=TradeType.SELL,
+                amount=self.order_amount,
+                price=order_price)
+            sell_candidate_adjusted = self.connector.budget_checker.adjust_candidate(sell_candidate, all_or_none=False)
+            if sell_candidate_adjusted.amount > 0:
+                self.log_with_clock(logging.INFO, f"Sell {sell_candidate_adjusted.amount} {self.maker_pair} "
+                                                  f"with the price {sell_candidate_adjusted.price}")
+                self.sell(
+                    self.connector_name,
+                    self.maker_pair,
+                    sell_candidate_adjusted.amount,
+                    sell_candidate_adjusted.order_type,
+                    sell_candidate_adjusted.price)
+                self.has_open_ask = True
 
-    # def create_and_process_taker_order_candidates(self, filled_side, filled_amount):
-    #     exchanged_amount = filled_amount
-    #
-    #
-    #     for i in range(2):
-    #         amount = self.get_order_amount_from_exchanged_amount(self.taker_pairs[filled_side][i],
-    #                                                              self.taker_order_side[filled_side][i],
-    #                                                              exchanged_amount)
-    #
-    #         self.taker_candidates[i] = self.create_order_candidate(self.taker_pairs[filled_side][i],
-    #                                                                self.taker_order_side[filled_side][i],
-    #                                                                amount)
-    #         # update amount for next cycle
-    #         if self.taker_order_side[filled_side][i]:
-    #             exchanged_amount = amount
-    #         else:
-    #             exchanged_amount = self.connector.get_quote_volume_for_base_amount(self.taker_pairs[filled_side][i],
-    #                                                                                self.taker_order_side[filled_side][i],
-    #                                                                                amount).result_volume
-    # def get_order_amount_from_exchanged_amount(self, pair, side, exchanged_amount) -> Decimal:
-    #     """
-    #     Calculates order amount using the amount that we want to exchange.
-    #     - If the side is buy then exchanged asset is a quote asset. Get base amount using the orderbook
-    #     - If the side is sell then exchanged asset is a base asset.
-    #     """
-    #     if side:
-    #         orderbook = self.connector.get_order_book(pair)
-    #         order_amount = self.get_base_amount_for_quote_volume(orderbook.ask_entries(), exchanged_amount)
-    #     else:
-    #         order_amount = exchanged_amount
-    #
-    #     return order_amount
-    #
-    # def create_order_candidate(self, pair, side, amount):
-    #     """
-    #     Creates order candidate. Checks the quantized amount
-    #     """
-    #     side = TradeType.BUY if side else TradeType.SELL
-    #     price = self.connector.get_price_for_volume(pair, side, amount).result_price
-    #     price_quantize = self.connector.quantize_order_price(pair, Decimal(price))
-    #     amount_quantize = self.connector.quantize_order_amount(pair, Decimal(amount))
-    #
-    #     if amount_quantize == Decimal("0"):
-    #         self.log_with_clock(logging.INFO, f"Order amount on {pair} is too low to place an order")
-    #         return None
-    #
-    #     return OrderCandidate(
-    #         trading_pair=pair,
-    #         is_maker=False,
-    #         order_type=OrderType.MARKET,
-    #         order_side=side,
-    #         amount=amount_quantize,
-    #         price=price_quantize)
-    #
-    # def adjust_order_candidate(self, order_candidate, multiple_trials_enabled) -> bool:
-    #     """
-    #     Checks order candidate balance and either places an order or sets a failure for the next trials
-    #     """
-    #     order_candidate_adjusted = self.connector.budget_checker.adjust_candidate(order_candidate, all_or_none=True)
-    #     if math.isclose(order_candidate.amount, Decimal("0"), rel_tol=1E-6):
-    #         self.logger().info(f"Order adjusted amount: {order_candidate.amount} on {order_candidate.trading_pair}, "
-    #                            f"too low to place an order")
-    #         if multiple_trials_enabled:
-    #             self.place_order_trials_count += 1
-    #             self.place_order_failure = True
-    #         return False
-    #     else:
-    #         is_buy = True if order_candidate.order_side == TradeType.BUY else False
-    #         self.place_order(self.connector_name,
-    #                          order_candidate.trading_pair,
-    #                          is_buy,
-    #                          order_candidate_adjusted.amount,
-    #                          order_candidate.order_type,
-    #                          order_candidate_adjusted.price)
-    #         return True
-    #
-    # def place_order(self,
-    #                 connector_name: str,
-    #                 trading_pair: str,
-    #                 is_buy: bool,
-    #                 amount: Decimal,
-    #                 order_type: OrderType,
-    #                 price=Decimal("NaN"),
-    #                 ):
-    #     if is_buy:
-    #         self.buy(connector_name, trading_pair, amount, order_type, price)
-    #     else:
-    #         self.sell(connector_name, trading_pair, amount, order_type, price)
-    #
-    #
-    # def check_and_cancel_open_orders(self):
-    #     for order in self.get_active_orders(connector_name=self.connector_name):
-    #         if order.is_buy:
-    #             if self.check_price_of_open_order():
-    #                 self.cancel(self.connector_name, order.trading_pair, order.client_order_id)
-    #                 self.has_open_bid = False
-    #         else:
-    #             if self.check_price_of_open_order():
-    #                 self.cancel(self.connector_name, order.trading_pair, order.client_order_id)
-    #                 self.has_open_ask = False
-    #
+    def calculate_taker_price(self, order_sides):
+        taker_1_price = self.connector.get_price(self.taker_pair_1, order_sides[0])
+        taker_1_price_ = self.connector.get_price(self.taker_pair_1, 1 - order_sides[0])
+        taker_2_price = self.connector.get_price(self.taker_pair_2, order_sides[1])
+        taker_2_price_ = self.connector.get_price(self.taker_pair_2, 1 - order_sides[1])
+        maker_price_bid_ = self.connector.get_price(self.maker_pair, False)
+        maker_price_ask_ = self.connector.get_price(self.maker_pair, True)
+        if self.taker_price_calculation_method == 1:
+            taker_price = taker_1_price * taker_2_price
+        elif self.taker_price_calculation_method == 2:
+            taker_price = taker_1_price / taker_2_price
+        else:
+            taker_price = taker_2_price / taker_1_price
+        return taker_price
+
+    def check_and_cancel_open_orders(self):
+        self.has_open_bid = False
+        self.has_open_ask = False
+        for order in self.get_active_orders(connector_name=self.connector_name):
+            if order.is_buy:
+                upper_price = self.taker_sell_price * Decimal(1 - self.min_spread / 100)
+                lower_price = self.taker_sell_price * Decimal(1 - self.max_spread / 100)
+                self.log_with_clock(logging.INFO, f"BUY order price {order.price} Range:"
+                                                  f"{lower_price} - {upper_price}")
+                if order.price > upper_price or order.price < lower_price:
+                    self.log_with_clock(logging.INFO, f"BUY order price {order.price} is not in the range "
+                                                      f"{lower_price} - {upper_price}. Cancelling order.")
+                    self.cancel(self.connector_name, order.trading_pair, order.client_order_id)
+                else:
+                    self.has_open_bid = True
+            else:
+                upper_price = self.taker_buy_price * Decimal(1 + self.max_spread / 100)
+                lower_price = self.taker_buy_price * Decimal(1 + self.min_spread / 100)
+                self.log_with_clock(logging.INFO, f"SELL order price {order.price} Range:"
+                                                  f"{lower_price} - {upper_price}")
+                if order.price > upper_price or order.price < lower_price:
+                    self.log_with_clock(logging.INFO, f"SELL order price {order.price} is not in the range "
+                                                      f"{lower_price} - {upper_price}. Cancelling order.")
+                    self.cancel(self.connector_name, order.trading_pair, order.client_order_id)
+                else:
+                    self.has_open_ask = True
