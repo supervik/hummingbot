8ca3a3f2243757fb1461b76609436781e1148422
supervik <vktsbk@gmail.com>
2023-03-06 19:42:38 +0300

    (feat) Candlesticks tails - write descriptions and refactor for better usage

--- scripts/candlesticks_tails.py
+++ scripts/candlesticks_tails.py
@@ -3,36 +3,34 @@ import os
 
 import pandas as pd
 import time
-import pandas_ta as ta
 import requests
-from hummingbot.connector.connector_base import ConnectorBase, Dict
 
 from hummingbot import data_path
-from hummingbot.connector.utils import split_hb_trading_pair
-from hummingbot.core.data_type.common import TradeType
-from hummingbot.core.data_type.order_candidate import OrderCandidate
-from hummingbot.core.event.events import BuyOrderCreatedEvent, OrderFilledEvent, SellOrderCreatedEvent
 from hummingbot.core.utils.async_utils import safe_ensure_future
-from hummingbot.data_feed.candles_feed.candles_factory import CandlesFactory
-from hummingbot.strategy.script_strategy_base import Decimal, OrderType, ScriptStrategyBase
+from hummingbot.strategy.script_strategy_base import Decimal, ScriptStrategyBase
 
 
 class CandleSticksTails(ScriptStrategyBase):
+    """
+    This script has 2 functions
+    - Fetch all trading pairs from the exchange with the help of hb all_trading_pairs method and saves them to csv file
+    - Get 1000 5 min candles for each trading pair and saves maximum up and down tails number into a csv file
+    """
     # Config params
-    connector_name: str = "binance"
+    connector_name: str = "kucoin"
     fake_pair = "SOL-USDT"
-    tail_pct_threshold = 1
-
-    fetch_all_trading_pairs = False
-    is_fetch_all_pairs_called = False
+    tail_pct_threshold = [0.5, 1, 1.5, 2, 2.5, 3]
 
     all_pairs = ""
-    status = "ACTIVE"
+    status = "FETCH_TAILS"
+    # status = "FETCH_ALL_PAIRS"
+    is_fetch_all_pairs_called = False
 
     markets = {connector_name: {fake_pair}}
 
     counter = 0
-    long_tails = {}
+    long_tails = pd.DataFrame([])
+    limit = 0
     # if pairs_threshold = 0 all available pairs will be calculated
     pairs_threshold = 0
 
@@ -44,9 +42,15 @@ class CandleSticksTails(ScriptStrategyBase):
         return self.connectors[self.connector_name]
 
     async def call_fetch_pairs(self):
+        """
+        Async method to get all trading pairs
+        """
         self.all_pairs = await self.connector.all_trading_pairs()
 
     def get_ohlc(self, connector_name, trading_pair):
+        """
+        Helper method to choose which method to use
+        """
         if connector_name == "binance":
             return self.get_ohlc_binance(trading_pair)
         if connector_name == "kucoin":
@@ -54,7 +58,9 @@ class CandleSticksTails(ScriptStrategyBase):
 
     def get_ohlc_binance(self, trading_pair):
         """
-        Fetches binance candle stick data and returns a dict OHLC
+        Fetches binance candle stick data and returns a list of dictionaries OHLC
+        Structure:
+        [{"open": 1.2, "high": 2.3, "low": 1, "close": 2.1}]
         """
         url = "https://api.binance.com/api/v3/klines"
         params = {"symbol": trading_pair.replace("-", ""),
@@ -67,19 +73,20 @@ class CandleSticksTails(ScriptStrategyBase):
 
     def get_ohlc_kucoin(self, trading_pair):
         """
-        Fetches binance candle stick data and returns a dict OHLC
+        Fetches kucoin candle stick data and returns a list of dictionaries OHLC
+        Structure:
+        [{"open": 1.2, "high": 2.3, "low": 1, "close": 2.1}]
+        Checks for api rate limits and trys again in case of error
         """
         url = "https://api.kucoin.com/api/v1/market/candles"
         end_time = int(self.current_timestamp) - 10
         start_time = int(end_time - 1000 * 60 * 5)
-        # self.log_with_clock(logging.INFO, f"start_time = {start_time}, end_time = {end_time}")
         params = {"symbol": trading_pair,
                   "type": "5min",
                   "startAt": start_time,
                   "endAt": end_time}
         records = requests.get(url=url, params=params).json()
         if records['code'] == "200000":
-            # self.log_with_clock(logging.INFO, f"trading_pair = {trading_pair}, records = {records}")
             return [{"open": Decimal(str(record[1])),
                      "high": Decimal(str(record[3])),
                      "low": Decimal(str(record[4])),
@@ -95,49 +102,51 @@ class CandleSticksTails(ScriptStrategyBase):
     def on_tick(self):
         if self.status == "NOT_ACTIVE":
             return
+        if self.status == "FETCH_ALL_PAIRS":
+            self.fetch_all_trading_pairs()
+            return
+        if self.status == "FETCH_TAILS":
+            self.all_pairs = pd.read_csv(os.path.join(data_path(), f"all_pairs_{self.connector_name}.csv"),
+                                         names=["pair"]).pair.tolist()
+            self.limit = self.pairs_threshold if self.pairs_threshold else len(self.all_pairs)
+            self.status = "FETCH_TAILS_STARTED"
 
-        if self.fetch_all_trading_pairs:
-            if not self.is_fetch_all_pairs_called:
-                safe_ensure_future(self.call_fetch_pairs())
-                self.is_fetch_all_pairs_called = True
-            self.log_with_clock(logging.INFO, f"all_pairs {self.all_pairs}")
-            if self.all_pairs:
-                csv_filename = f"all_pairs_{self.connector_name}.csv"
-                csv_path = os.path.join(data_path(), csv_filename)
-                csv_df = pd.DataFrame(self.all_pairs)
-                csv_df.to_csv(csv_path, index=False)
-                self.all_pairs = False
-                self.log_with_clock(logging.INFO, f"Fetching all pairs finished")
-                self.status = "NOT_ACTIVE"
+        if self.counter > self.limit - 1:
+            self.finish_and_save_result()
             return
 
-        filename = f"all_pairs_{self.connector_name}.csv"
-        path = os.path.join(data_path(), filename)
-        df = pd.read_csv(path, names=["pair"])
-        all_trading_pairs = df.pair.tolist()
+        self.process_pair(self.all_pairs[self.counter])
 
-        limit = self.pairs_threshold if self.pairs_threshold else len(all_trading_pairs)
-
-        if self.counter > limit - 1:
-            self.log_with_clock(logging.INFO, f"Long tail calculation finished")
-            long_tails_all_sorted = dict(sorted(self.long_tails.items(), key=lambda item: item[1]))
-
-            filename = f"long_tails_{self.connector_name}.csv"
-            path = os.path.join(data_path(), filename)
-            df = pd.DataFrame(long_tails_all_sorted.items(), columns=['Pair', 'Tails_number'])
-            df.to_csv(path, index=False)
-            self.log_with_clock(logging.INFO, f"long_tails_all_sorted {long_tails_all_sorted}")
+    def fetch_all_trading_pairs(self):
+        """
+        Get all trading pairs from the exchange and saves them to a csv file
+        - Checks if the request to fetch trading pairs was sent
+        - Waits while we got all trading pairs and saves them to a file
+        - Set status to not active
+        """
+        if not self.is_fetch_all_pairs_called:
+            safe_ensure_future(self.call_fetch_pairs())
+            self.is_fetch_all_pairs_called = True
+        self.log_with_clock(logging.INFO, f"all_pairs {self.all_pairs}")
+        if self.all_pairs:
+            csv_filename = f"all_pairs_{self.connector_name}.csv"
+            csv_path = os.path.join(data_path(), csv_filename)
+            csv_df = pd.DataFrame(self.all_pairs)
+            csv_df.to_csv(csv_path, index=False)
+            self.all_pairs = False
+            self.log_with_clock(logging.INFO, f"Fetching all pairs finished")
             self.status = "NOT_ACTIVE"
-            return
 
-        pair = all_trading_pairs[self.counter]
-        self.log_with_clock(logging.INFO, f"Processing {self.counter + 1} / {limit} pair: {pair}")
+    def process_pair(self, pair):
+        """
+        Fetches OHLC for a pair, calculates the number of tails that exceeds the threshold and saves to self.long_tails
+        """
+        self.log_with_clock(logging.INFO, f"Processing {self.counter + 1} / {self.limit} pair: {pair}")
         candles_df = self.get_ohlc(self.connector_name, pair)
         if not candles_df:
             self.counter += 1
             return
         candles_df = pd.DataFrame(candles_df)
-        # self.log_with_clock(logging.INFO, f"candles_df = {candles_df}")
         candles_df_up = candles_df[candles_df.close >= candles_df.open]
         candles_df_down = candles_df[candles_df.close < candles_df.open]
         candles_df_up = candles_df_up.assign(up_tail=100 * (candles_df_up['high'] - candles_df_up['close']) / candles_df_up['close'])
@@ -149,10 +158,25 @@ class CandleSticksTails(ScriptStrategyBase):
         candles_df = pd.concat(candles_df)
         candles_df = candles_df[['up_tail', 'down_tail']]
         df_max_tails = candles_df.max(axis=1)
-        df_max_tails = df_max_tails[df_max_tails >= self.tail_pct_threshold]
-        long_tails = df_max_tails.count()
 
-        self.long_tails[pair] = long_tails
+        data_list = [pair]
+
+        for threshold in self.tail_pct_threshold:
+            df_max_tails = df_max_tails[df_max_tails >= threshold]
+            data_list.append(df_max_tails.count())
+
+        data_df = pd.DataFrame([data_list])
+        self.long_tails = pd.concat([self.long_tails, data_df])
         self.counter += 1
+
+    def finish_and_save_result(self):
+        """
+        Saves the tails dataframe to a csv file and sets the status to NOT_ACTIVE
+        """
+        path = os.path.join(data_path(), f"long_tails_{self.connector_name}.csv")
+        self.long_tails.to_csv(path, index=False)
+        self.log_with_clock(logging.INFO, f"Long tail calculation finished")
+        self.log_with_clock(logging.INFO, f"long_tails df: {self.long_tails}")
+        self.status = "NOT_ACTIVE"
 
 
