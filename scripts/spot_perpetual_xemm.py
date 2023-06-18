f9e303292ac2de47f3f97044b6cb5a2cb18b8092
supervik <vktsbk@gmail.com>
2023-06-19 00:04:27 +0300

    (feat) Spot perpetual XEMM - check positions, add maker and taker quantization, add order_id as filled event identifier

--- scripts/spot_perpetual_xemm.py
+++ scripts/spot_perpetual_xemm.py
@@ -3,23 +3,26 @@ import logging
 import pandas as pd
 
 from hummingbot.connector.utils import split_hb_trading_pair
-from hummingbot.core.data_type.common import TradeType, PositionAction
+from hummingbot.core.data_type.common import TradeType, PositionAction, PositionSide
 from hummingbot.core.data_type.order_candidate import OrderCandidate, PerpetualOrderCandidate
 from hummingbot.core.event.events import BuyOrderCreatedEvent, OrderFilledEvent, SellOrderCreatedEvent
 from hummingbot.strategy.script_strategy_base import Decimal, OrderType, ScriptStrategyBase
 
 
 class SpotPerpetualXEMM(ScriptStrategyBase):
+    """
+
+    """
     # Config params
-    maker_connector_name: str = "gate_io"
+    maker_connector_name: str = "kucoin"
     taker_connector_name: str = "gate_io_perpetual"
     maker_pair: str = "IGU-USDT"
     taker_pair: str = "IGU-USDT"
     # is_maker_spot = True
 
-    order_amount_in_quote = Decimal("10")
-    open_spread_bps = 200
-    min_spread_bps = 100
+    order_amount_in_quote = Decimal("2")
+    spread_bps = 60
+    min_spread_bps = 30
     max_order_age = 120
 
     slippage_buffer_spread_bps = 200
@@ -27,6 +30,9 @@ class SpotPerpetualXEMM(ScriptStrategyBase):
 
     # class parameters
     status = "NOT_INIT"
+    maker_order_ids = {}
+    maker_order_ids_clean_interval = 30 * 60
+    maker_order_ids_clean_timestamp = 0
     order_amount_in_base = 0
     sell_order_amount = 0
     buy_order_amount = 0
@@ -66,9 +72,9 @@ class SpotPerpetualXEMM(ScriptStrategyBase):
 
         if self.status == "NOT_ACTIVE":
             return
-        #
-        # if self.check_kill_switch_balance():
-        #     return
+
+        if self.current_timestamp > self.maker_order_ids_clean_timestamp:
+            self.clean_maker_order_ids()
 
         self.calculate_maker_order_amount()
 
@@ -94,41 +100,33 @@ class SpotPerpetualXEMM(ScriptStrategyBase):
         self.maker_base_asset, self.maker_quote_asset = split_hb_trading_pair(self.maker_pair)
         self.taker_base_asset, self.taker_quote_asset = split_hb_trading_pair(self.taker_pair)
 
-    # def check_kill_switch_balance(self):
-    #     if self.buy_on_maker:
-    #         base_asset, quote_asset = self.maker_pair.split("-")
-    #         base_amount = self.maker_connector.get_balance(base_asset)
-    #         if base_amount >= self.kill_switch_balance:
-    #             self.logger().info(f"Kill switch balance reached. Stop trading")
-    #             self.cancel_all_orders()
-    #             self.status = "NOT_ACTIVE"
-    #             return True
-    #     return False
+    def clean_maker_order_ids(self):
+        updated_maker_order_ids = {}
+        for order_id, timestamp in self.maker_order_ids.items():
+            if timestamp > self.current_timestamp - self.maker_order_ids_clean_interval:
+                updated_maker_order_ids[order_id] = timestamp
+
+        self.maker_order_ids = updated_maker_order_ids
+        self.maker_order_ids_clean_timestamp = self.current_timestamp + self.maker_order_ids_clean_interval
 
     def cancel_all_orders(self):
         for order in self.get_active_orders(self.maker_connector_name):
             self.cancel(self.maker_connector_name, order.trading_pair, order.client_order_id)
 
-    # def calculate_maker_order_amount(self):
-    #     amount_left = self.total_amount - self.processed_amount
-    #     self.maker_order_amount = min(amount_left, self.max_order_amount)
-    #     quantized_amount = self.maker_connector.quantize_order_amount(self.maker_pair, self.maker_order_amount)
-    #     if quantized_amount == Decimal("0"):
-    #         final_price = round(self.processed_amount_quote / self.processed_amount, 8) if self.processed_amount else -1
-    #         self.logger().info(f"Left amount {self.maker_order_amount} is less than minimum threshold. Stop the bot")
-    #         self.logger().info(
-    #             f"Total amount processed = {self.processed_amount}.  Amount_quote = {self.processed_amount_quote} "
-    #             f"Final price = {final_price}")
-    #         self.status = "NOT_ACTIVE"
-
     def calculate_maker_order_amount(self):
+        # get sell amount
         base_amount = self.maker_connector.get_balance(self.maker_base_asset)
-        base_amount_quantized = self.maker_connector.quantize_order_amount(self.maker_pair, base_amount)
+        self.sell_order_amount = self.quantize_amount_on_maker_and_taker(base_amount)
 
+        # get buy amount
         mid_price = self.maker_connector.get_mid_price(self.maker_pair)
         self.order_amount_in_base = self.order_amount_in_quote / mid_price
-        self.sell_order_amount = base_amount_quantized
-        self.buy_order_amount = self.order_amount_in_base
+        self.buy_order_amount = self.quantize_amount_on_maker_and_taker(self.order_amount_in_base)
+
+    def quantize_amount_on_maker_and_taker(self, amount):
+        amount_quantized_on_maker = self.maker_connector.quantize_order_amount(self.maker_pair, amount)
+        amount_quantized_on_taker = self.taker_connector.quantize_order_amount(self.taker_pair, amount)
+        return min(amount_quantized_on_maker, amount_quantized_on_taker)
 
     def calculate_taker_hedging_price(self):
         self.taker_sell_hedging_price = self.taker_connector.get_price_for_volume(self.taker_pair, False,
@@ -139,9 +137,9 @@ class SpotPerpetualXEMM(ScriptStrategyBase):
         mid_maker_price = self.maker_connector.get_mid_price(self.maker_pair)
 
         mid_dif = mid_taker_price - mid_maker_price
-        self.logger().info(
-            f"mid_maker_price = {mid_maker_price} mid_taker_price = {mid_taker_price}, mid_dif = {mid_dif}, "
-            f"self.taker_sell_hedging_price = {self.taker_sell_hedging_price}")
+        # self.logger().info(
+        #     f"mid_maker_price = {mid_maker_price} mid_taker_price = {mid_taker_price}, mid_dif = {mid_dif}, "
+        #     f"self.taker_sell_hedging_price = {self.taker_sell_hedging_price}")
         if mid_dif > 0:
             self.taker_sell_hedging_price -= mid_dif
 
@@ -182,24 +180,52 @@ class SpotPerpetualXEMM(ScriptStrategyBase):
 
         maker_order_adjusted = self.maker_connector.budget_checker.adjust_candidate(maker_order, all_or_none=False)
         if maker_order_adjusted.amount != Decimal("0"):
-            self.send_order_to_exchange(candidate=maker_order_adjusted, connector_name=self.maker_connector_name)
+            order_id = self.send_order_to_exchange(candidate=maker_order_adjusted, connector_name=self.maker_connector_name)
+            if order_id:
+                self.maker_order_ids[order_id] = self.current_timestamp
 
     def send_order_to_exchange(self, candidate, connector_name):
         if candidate.order_side == TradeType.SELL:
-            self.sell(connector_name, candidate.trading_pair, candidate.amount, candidate.order_type,
+            order_id = self.sell(connector_name, candidate.trading_pair, candidate.amount, candidate.order_type,
                       candidate.price, PositionAction.OPEN)
         else:
-            self.buy(connector_name, candidate.trading_pair, candidate.amount, candidate.order_type,
+            order_id = self.buy(connector_name, candidate.trading_pair, candidate.amount, candidate.order_type,
                      candidate.price, PositionAction.OPEN)
+
+        return order_id
+
+    # def test_open_perpetual_order(self):
+    #     amount = Decimal("20")
+    #     taker_price = self.taker_connector.get_price_for_volume(self.taker_pair, True, amount).result_price
+    #     taker_price_with_slippage = taker_price * Decimal(1 + self.slippage_buffer_spread_bps / 10000)
+    #     self.logger().info(f"taker_price = {taker_price}, taker_price_with_slippage = {taker_price_with_slippage}")
+    #     taker_candidate = PerpetualOrderCandidate(trading_pair=self.taker_pair, is_maker=False,
+    #                                               order_type=OrderType.LIMIT,
+    #                                               order_side=TradeType.BUY, amount=amount,
+    #                                               price=taker_price_with_slippage, leverage=self.leverage)
+    #
+    #     taker_candidate_adjusted = self.taker_connector.budget_checker.adjust_candidate(taker_candidate,
+    #                                                                                     all_or_none=True)
+    #     if taker_candidate_adjusted.amount != Decimal("0"):
+    #         self.send_order_to_exchange(candidate=taker_candidate_adjusted, connector_name=self.taker_connector_name)
+    #         self.logger().info(f"send_order_to_exchange. Candidate adjusted {taker_candidate_adjusted}")
+    #
+    # def get_open_positions(self):
+    #     open_positions = self.taker_connector.account_positions
+    #     self.logger().info(f"open_positions = {open_positions}")
+    #     for key, position in open_positions.items():
+    #         self.logger().info(f"position = {position}")
+    #         position_amount = position.amount
+    #         self.logger().info(f"Current short_position_amount = {position_amount}")
 
     def is_active_maker_order(self, event: OrderFilledEvent):
         """
         Helper function that checks if order is an active order on the maker exchange
         """
-        if (event.trade_type == TradeType.BUY and self.maker_side == TradeType.BUY) or \
-                (event.trade_type == TradeType.SELL and self.maker_side == TradeType.SELL):
+        if event.order_id in self.maker_order_ids:
             return True
-        return False
+        else:
+            return False
 
     def did_fill_order(self, event: OrderFilledEvent):
         exchange = "Taker"
@@ -218,6 +244,7 @@ class SpotPerpetualXEMM(ScriptStrategyBase):
         amount_total = Decimal("0")
         for event in self.filled_event_buffer:
             amount_total += event.amount
+            event_side = event.trade_type
 
         self.logger().info(f"amount_total = {amount_total}")
         quantized_amount_total = self.taker_connector.quantize_order_amount(self.taker_pair, amount_total)
@@ -228,24 +255,35 @@ class SpotPerpetualXEMM(ScriptStrategyBase):
             return
 
         self.cancel_all_orders()
-        if self.maker_side == TradeType.BUY:
+
+        if event_side == TradeType.BUY:
             taker_side = TradeType.SELL
-            taker_price = self.taker_connector.get_price_for_volume(self.maker_pair, False,
-                                                                    quantized_amount_total).result_price
+            taker_amount = quantized_amount_total
+            taker_price = self.taker_connector.get_price_for_volume(self.maker_pair, False, taker_amount).result_price
             taker_price_with_slippage = taker_price * Decimal(1 - self.slippage_buffer_spread_bps / 10000)
             self.logger().info(f"Sending sell on taker with price {taker_price}. "
                                f"Price with slippage {taker_price_with_slippage}")
+
         else:
+            # get amount of short positions to close them
+            open_positions = self.taker_connector.account_positions
+            short_position_amount = Decimal("0")
+            self.logger().info(f"open_positions = {open_positions}")
+            for key, position in open_positions.items():
+                if position.position_side == PositionSide.SHORT:
+                    short_position_amount = abs(position.amount)
+                    self.logger().info(f"Current short_position_amount = {short_position_amount}")
+
             taker_side = TradeType.BUY
-            taker_price = self.taker_connector.get_price_for_volume(self.maker_pair, True,
-                                                                    quantized_amount_total).result_price
+            taker_amount = min(short_position_amount, amount_total)
+            taker_price = self.taker_connector.get_price_for_volume(self.taker_pair, True, taker_amount).result_price
             taker_price_with_slippage = taker_price * Decimal(1 + self.slippage_buffer_spread_bps / 10000)
             self.logger().info(f"Sending buy on taker with price {taker_price}."
                                f" Price with slippage {taker_price_with_slippage}")
 
         taker_candidate = PerpetualOrderCandidate(trading_pair=self.taker_pair, is_maker=False,
                                                   order_type=OrderType.LIMIT,
-                                                  order_side=taker_side, amount=amount_total,
+                                                  order_side=taker_side, amount=taker_amount,
                                                   price=taker_price_with_slippage, leverage=self.leverage)
 
         taker_candidate_adjusted = self.taker_connector.budget_checker.adjust_candidate(taker_candidate,
@@ -273,6 +311,7 @@ class SpotPerpetualXEMM(ScriptStrategyBase):
         lines = []
 
         lines.extend(["", "  Strategy status:"] + ["    " + self.status])
+        # lines.extend(["", "  order_ids:"] + ["    " + str(self.maker_order_ids)])
 
         lines.extend(["", "  Maker side:"] + ["    " + str(self.maker_side)])
         lines.extend(["", "  taker_sell_hedging_price:"] + ["    " + str(self.taker_sell_hedging_price)])
@@ -293,11 +332,11 @@ class SpotPerpetualXEMM(ScriptStrategyBase):
         """
         Returns a custom data frame of all active maker orders for display purposes
         """
-        columns = ["Market", "Pair", "Side", "Price", "Size", "Min price", "Max price", "Spread", "Age"]
+        columns = ["Market", "Pair", "Side", "Price", "Size", "Spread", "Age"]
         data = []
         for connector_name, connector in self.connectors.items():
             for order in self.get_active_orders(connector_name):
-                mid_price = self.connector.get_mid_price(order.trading_pair)
+                mid_price = connector.get_mid_price(order.trading_pair)
                 if order.is_buy:
                     spread_mid = (mid_price - order.price) / mid_price * 100
                 else:
@@ -310,8 +349,6 @@ class SpotPerpetualXEMM(ScriptStrategyBase):
                     "buy" if order.is_buy else "sell",
                     float(order.price),
                     float(order.quantity),
-                    float(self.lower_price),
-                    float(self.upper_price),
                     float(round(spread_mid, 2)),
                     age_txt
                 ])
